4eef2e98eb1a6b15a404ca4e3a0b46e89e2e3975
Kaique Fernando <109758907+kaiquef30@users.noreply.github.com>
2023-10-18 21:55:35 -0300

    Update SecurityConfigurations.java

--- src/main/java/com/projectmicrosoft/microsoft/api/security/SecurityConfigurations.java
+++ src/main/java/com/projectmicrosoft/microsoft/api/security/SecurityConfigurations.java
@@ -2,10 +2,10 @@ package com.projectmicrosoft.microsoft.api.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -24,10 +24,10 @@ public class SecurityConfigurations {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return  httpSecurity
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/auth/**", "/v3/api-docs/**","/swagger-ui/**", "/sla").permitAll()
+                        .requestMatchers("/api/auth/**", "/v3/api-docs/**","/swagger-ui/**", "/sla").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
