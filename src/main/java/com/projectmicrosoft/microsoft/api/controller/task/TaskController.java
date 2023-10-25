1db6a4bec514d4868ce7de465ce5b6f47388886b
Kaique Fernando <109758907+kaiquef30@users.noreply.github.com>
2023-10-25 08:59:28 -0300

    Update TaskController.java

--- src/main/java/com/projectmicrosoft/microsoft/api/controller/task/TaskController.java
+++ src/main/java/com/projectmicrosoft/microsoft/api/controller/task/TaskController.java
@@ -3,11 +3,16 @@ package com.projectmicrosoft.microsoft.api.controller.task;
 
 import com.projectmicrosoft.microsoft.api.dto.TaskDto;
 import com.projectmicrosoft.microsoft.api.security.AuthenticatedUser;
+import com.projectmicrosoft.microsoft.exception.InvalidAttachmentException;
 import com.projectmicrosoft.microsoft.exception.TaskNotFoundException;
 import com.projectmicrosoft.microsoft.exception.messages.ClientMessageConfig;
 import com.projectmicrosoft.microsoft.exception.messages.TaskMessageConfig;
 import com.projectmicrosoft.microsoft.model.Task;
 import com.projectmicrosoft.microsoft.service.TaskService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -30,48 +35,65 @@ public class TaskController {
         this.taskMessageConfig = taskMessageConfig;
     }
 
-    @AuthenticatedUser(requiredRoles = "ADMIN")
+    @AuthenticatedUser(requiredRoles = {"ADMIN"})
+    @Operation(summary = "Create task")
+    @ApiResponse(responseCode = "201", description = "Task created successfully.",
+            content = {@Content(schema = @Schema(implementation = TaskDto.class))})
+    @ApiResponse(responseCode = "500", description = "Error processing attachments.", content = @Content)
+    @ApiResponse(responseCode = "400", description = "Invalid file type or size.", content = @Content)
     @PostMapping("/create")
     public ResponseEntity<?> createTask(@RequestBody TaskDto taskDto,
                                         @RequestParam(value = "attachments", required = false)
                                         MultipartFile[] attachments) {
         try {
-            taskService.createTask(taskDto, attachments);
-            return ResponseEntity.status(HttpStatus.CREATED).body(taskMessageConfig.getTaskCreatedSuccessfully());
+            Task createdTask = taskService.createTask(taskDto, attachments);
+            return ResponseEntity.status(HttpStatus.CREATED).body(createdTask);
         } catch (IOException e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body(taskMessageConfig.getErrorProcessingAttachments());
+        } catch (InvalidAttachmentException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(taskMessageConfig.getInvalidAttachment());
         }
     }
 
-    @AuthenticatedUser(requiredRoles = "ADMIN")
+    @AuthenticatedUser(requiredRoles = {"ADMIN"})
+    @Operation(summary = "Edit a task")
+    @ApiResponse(responseCode = "200", description = "Task edited successfully.",
+            content = {@Content(schema = @Schema(implementation = TaskDto.class))})
+    @ApiResponse(responseCode = "500", description = "Error processing attachments.", content = @Content)
     @PutMapping("/update/{taskId}")
     public ResponseEntity<?> updateTask(@PathVariable Long taskId, @RequestBody TaskDto taskDto,
                                         @RequestParam(value = "attachments", required = false)
                                         MultipartFile[] attachments) {
         try {
-            return ResponseEntity.status(HttpStatus.OK).body(taskService.updateTask(taskId, taskDto, attachments));
+            Task updatedTask = taskService.updateTask(taskId, taskDto, attachments);
+            return ResponseEntity.status(HttpStatus.OK).body(updatedTask);
         } catch (IOException e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body(taskMessageConfig.getErrorProcessingAttachments());
-        } catch (TaskNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(taskMessageConfig.getTaskNotFound());
         }
     }
 
-    @AuthenticatedUser(requiredRoles = "USER")
+    @AuthenticatedUser(requiredRoles = {"USER"})
+    @Operation(summary = "Search task by id")
+    @ApiResponse(responseCode = "200", description = "Task found successfully.",
+            content = @Content)
+    @ApiResponse(responseCode = "404", description = "Task not found.", content = @Content)
     @GetMapping("/search/{taskId}")
     public ResponseEntity<?> getTaskById(@PathVariable Long taskId) {
         Task taskFound;
         try {
             taskFound = taskService.getTaskById(taskId);
-            return ResponseEntity.status(HttpStatus.FOUND).body(taskFound);
+            return ResponseEntity.status(HttpStatus.OK).body(taskFound);
         } catch (TaskNotFoundException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(taskMessageConfig.getTaskNotFound());
         }
     }
 
-    @AuthenticatedUser(requiredRoles = "ADMIN")
+    @AuthenticatedUser(requiredRoles = {"ADMIN"})
+    @Operation(summary = "Delete task")
+    @ApiResponse(responseCode = "204", description = "Task deleted successfully.", content = @Content)
+    @ApiResponse(responseCode = "404", description = "Task not found.", content = @Content)
     @DeleteMapping("/{taskId}")
     public ResponseEntity<?> deleteTask(@PathVariable Long taskId) {
         try {
