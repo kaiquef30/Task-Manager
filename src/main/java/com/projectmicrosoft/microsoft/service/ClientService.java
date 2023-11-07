790c84e7ecf20e8eeb6b0b334634c1bfd90a427d
Kaique Fernando <109758907+kaiquef30@users.noreply.github.com>
2023-11-07 11:50:40 -0300

    Update ClientService.java

--- src/main/java/com/projectmicrosoft/microsoft/service/ClientService.java
+++ src/main/java/com/projectmicrosoft/microsoft/service/ClientService.java
@@ -7,6 +7,8 @@ import com.projectmicrosoft.microsoft.model.Client;
 import com.projectmicrosoft.microsoft.repository.ClientRepository;
 import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -26,22 +28,26 @@ public class ClientService {
 
 
     @Transactional
+    @CacheEvict(value = "clientCache", allEntries = true)
     public Client registerClient(ClientDTO clientDto) {
         Client client = modelMapper.map(clientDto, Client.class);
         return clientRepository.save(client);
     }
 
 
+    @Cacheable(value = "clientCache", key = "#clientId")
     public Optional<Client> getClientById(Long clientId) {
         return clientRepository.findById(clientId);
     }
 
 
+    @Cacheable(value = "clientCache", key = "T(org.springframework.util.ClassUtils).getShortName('Client')")
     public List<Client> getAllClients() {
         return clientRepository.findAll();
     }
 
     @Transactional
+    @CacheEvict(value = "clientCache", key = "#clientId")
     public Client updateClient(Long clientId, ClientDTO clientDto) throws ClientNotFoundException {
         Optional<Client> existingClientOptional = clientRepository.findById(clientId);
 
@@ -55,6 +61,7 @@ public class ClientService {
     }
 
     @Transactional
+    @CacheEvict(value = "clientCache", key = "#clientId")
     public void deleteClient(Long clientId) throws ClientNotFoundException {
         if (clientRepository.existsById(clientId)) {
             clientRepository.deleteById(clientId);
